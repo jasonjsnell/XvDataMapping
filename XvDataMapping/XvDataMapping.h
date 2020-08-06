c18bd176e13f700b90cc82f91891abe92cdc56c8
jasonjsnell <jasonjsnell@users.noreply.github.com>
2020-08-06 09:13:32 -0500

    Initial Commit

---
+++ XvDataMapping/XvDataMapping.h
@@ -0,0 +1,19 @@
+//
+//  XvDataMapping.h
+//  XvDataMapping
+//
+//  Created by Jason Snell on 8/6/20.
+//  Copyright © 2020 Jason Snell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XvDataMapping.
+FOUNDATION_EXPORT double XvDataMappingVersionNumber;
+
+//! Project version string for XvDataMapping.
+FOUNDATION_EXPORT const unsigned char XvDataMappingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvDataMapping/PublicHeader.h>
+
+
