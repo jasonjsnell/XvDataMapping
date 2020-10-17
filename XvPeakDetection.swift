489f67b3aaa6646098e4b8a69182b42eeb9c669c
jasonjsnell <jasonjsnell@users.noreply.github.com>
2020-10-17 15:11:10 -0500

    Created XvGraph, quick class for graphing data

--- XvPeakDetection.swift
+++ XvPeakDetection.swift
@@ -11,42 +11,175 @@ import Foundation
 
 public struct XvPeakDetectionPacket {
     
-    init(peaks:[Double], averagedValues:[Double], standardDeviationValues:[Double]) {
+    init(peaks:[Int], averagedValues:[Double], deviationValues:[Double]) {
         self.peaks = peaks
         self.averagedValues = averagedValues
-        self.standardDeviationValues = standardDeviationValues
+        self.deviationValues = deviationValues
     }
-    var peaks:[Double]
-    var averagedValues:[Double]
-    var standardDeviationValues:[Double]
+    public var peaks:[Int]
+    public var averagedValues:[Double]
+    public var deviationValues:[Double]
 }
 
 public class XvPeakDetection {
     
     public init(
+        bins:Int,
         threshold:Double,
         averagingLag:Int,
-        lagDeviation:Int,
+        deviationLag:Int,
         averagingInfluence:Double,
         deviationInfluence:Double
     ){
         
+        //capture vars
+        _bins = bins
         _threshold = threshold
         _averagingLag = averagingLag
-        _lagDeviation = lagDeviation
+        _deviationLag = deviationLag
         _averagingInfluence = averagingInfluence
         _deviationInfluence = deviationInfluence
+        
+        //init empty signal
+        _signal = []
+        
+        //error checking
+        if (_averagingLag > _bins || _deviationLag > _bins) {
+            print("XvPeakDetection: Error: Lags (", _averagingLag, _deviationLag, ") can't be more than the bin length", _bins)
+            fatalError()
+        }
     }
     
-    // Smooth z-score thresholding filter
-    public func process(y: [Double]) -> XvPeakDetectionPacket? {
+    fileprivate let _bins:Int
+    fileprivate var _signal:[Double]
+    
+    //MARK: - Process value
+    //process one value at a time
+    public func process(value:Double) -> XvPeakDetectionPacket? {
+        
+        //add value to end of the signal
+        _signal.append(value)
+        
+        //buffer signal
+        if (_bufferSignal()){
+            
+            //then detect peaks
+            return _peakDetection()
+        }
+        
+        return nil
+    }
+    
+    //MARK: Process packet
+    //this is a small array of several values, often used in device communication
+    public func process(packet:[Double]) -> XvPeakDetectionPacket? {
+        
+        //append entire packet
+        _signal += packet
+        
+        //buffer signal
+        if (_bufferSignal()){
+            
+            //then detect peaks
+            return _peakDetection()
+        }
+        
+        return nil
+    }
+    
+    //MARK: Process signal
+    //this is an array the same length as the bins
+    public func process(stream:[Double]) -> XvPeakDetectionPacket? {
+        
+        //if incoming signal is the size of bins
+        if (stream.count == _bins) {
+            
+            //save and process
+            _signal = stream
+            return _peakDetection()
+        
+        } else {
+            print("XvPeakDetection: Error: process: signal length", stream.count, "is not the same as bin length", _bins)
+            return nil
+        }
+    }
+    
+    
+    //MARK: - Peak detection -
+    fileprivate func _peakDetection() -> XvPeakDetectionPacket?{
 
+        /*
+        //MARK: Error checking
+        //confirm the lag times within the range of the signal array
+        if (_averagingLag > _signal.count || _deviationLag > _signal.count){
+            print("XvPeakDetection: Lag time cannot be longer than the sample buffer size", _signal.count)
+            return nil
+        }
+        
         // Create arrays
-        var peaks:[Double] = [Double](repeating: 0.0, count: y.count)
-        var filteredYmean:[Double] = [Double](repeating: 0.0, count: y.count)
-        var filteredYstd:[Double] = [Double](repeating: 0.0, count: y.count)
-        var avgFilter:[Double] = [Double](repeating: 0.0, count: y.count)
-        var stdFilter:[Double] = [Double](repeating: 0.0, count: y.count)
+        var peaks:[Int]            = [Int](repeating: 0, count: _signal.count)
+        var filteredYAvg:[Double]  = [Double](repeating: 0.0, count: _signal.count)
+        var filteredYDev:[Double]  = [Double](repeating: 0.0, count: _signal.count)
+        var avgFilter:[Double]     = [Double](repeating: 0.0, count: _signal.count)
+        var devFilter:[Double]     = [Double](repeating: 0.0, count: _signal.count)
+
+        // Initialise variables
+        for i in 0..._averagingLag-1 {
+            peaks[i] = 0 // TODO: why is this set to zero again?
+            filteredYAvg[i] = _signal[i]
+            filteredYDev[i] = _signal[i] //TODO: why is deviation filter the length of the average filter?
+        }
+    
+        // Start filter
+        avgFilter[_averagingLag-1] = getMean(array: subArray(array: peaks, s: 0, e: _averagingLag-1))
+        devFilter[_deviationLag-1] = getStandardDeviation(array: subArray(array: peaks, s: 0, e: _deviationLag-1))
+
+        for i in max(_averagingLag, _deviationLag)...peaks.count-1 {
+            
+            if abs(peaks[i] - avgFilter[i-1]) > _threshold * devFilter[i-1] {
+                
+                if peaks[i] > avgFilter[i-1] {
+                    peaks[i] = 1  // Positive signal
+                } else {
+                    peaks[i] = -1 // Negative signal
+                }
+                
+                filteredYAvg[i] = _averagingInfluence * peaks[i] + (1 - _averagingInfluence) * filteredYAvg[i-1]
+                filteredYDev[i] = _deviationInfluence * peaks[i] + (1 - _deviationInfluence) * filteredYDev[i-1]
+                
+            } else {
+                // Not a peak
+                peaks[i] = 0
+                filteredYAvg[i] = _signal[i]
+                filteredYDev[i] = _signal[i]
+                
+            }
+            
+            // Adjust the filters
+            avgFilter[i] = getMean(array: subArray(array: filteredYAvg, s: i-_averagingLag, e: i))
+            devFilter[i] = getStandardDeviation(array: subArray(array: filteredYDev, s: i-_deviationLag, e: i))
+        }
+
+        return XvPeakDetectionPacket(peaks: peaks, averagedValues: avgFilter, deviationValues: devFilter)
+        */
+        return nil
+    }
+    
+    public func ThresholdingAlgo(y: [Double]) -> XvPeakDetectionPacket? {
+
+        //MARK: Error checking
+        //confirm the lag times within the range of the signal array
+        if (_averagingLag > y.count || _deviationLag > y.count){
+            print("XvPeakDetection: Lag time cannot be longer than the sample buffer size", y.count)
+            return nil
+        }
+        
+        // Create arrays
+        var peaks   = Array(repeating: 0, count: y.count)
+        var filteredYmean = Array(repeating: 0.0, count: y.count)
+        var filteredYstd = Array(repeating: 0.0, count: y.count)
+        var avgFilter = Array(repeating: 0.0, count: y.count)
+        var devFilter = Array(repeating: 0.0, count: y.count)
 
         // Initialise variables
         for i in 0..._averagingLag-1 {
@@ -55,40 +188,31 @@ public class XvPeakDetection {
             filteredYstd[i] = y[i]
         }
 
-        //confirm the lag times within the range of the signal array
-        if (_averagingLag > y.count || _lagDeviation > y.count){
-            print("XvPeakDetection: Lag time cannot be longer than the sample buffer size")
-            return nil
-        }
-        
         // Start filter
-        avgFilter[_averagingLag-1] = getMedian(array: subArray(array: y, s: 0, e: _averagingLag-1))
-        stdFilter[_lagDeviation-1] = standardDeviation(array: subArray(array: y, s: 0, e: _lagDeviation-1))
+        avgFilter[_averagingLag-1] = getMean(array: subArray(array: y, s: 0, e: _averagingLag-1))
+        devFilter[_deviationLag-1] = getStandardDeviation(array: subArray(array: y, s: 0, e: _deviationLag-1))
 
-        for i in max(_averagingLag, _lagDeviation)...y.count-1 {
-            
-            if abs(y[i] - avgFilter[i-1]) > threshold*stdFilter[i-1] {
-                
+        for i in max(_averagingLag,_deviationLag)...y.count-1 {
+            if abs(y[i] - avgFilter[i-1]) > _threshold * devFilter[i-1] {
                 if y[i] > avgFilter[i-1] {
                     peaks[i] = 1      // Positive signal
                 } else {
                     peaks[i] = -1       // Negative signal
                 }
-                
-                filteredYmean[i] = _averagingInfluence * y[i] + (1 - _averagingInfluence) * filteredYmean[i-1]
-                filteredYstd[i] = _deviationInfluence * y[i] + (1 - _deviationInfluence) * filteredYstd[i-1]
+                filteredYmean[i] = _averagingInfluence*y[i] + (1-_averagingInfluence)*filteredYmean[i-1]
+                filteredYstd[i] = _deviationInfluence*y[i] + (1-_deviationInfluence)*filteredYstd[i-1]
             } else {
-                // No signal
-                return nil
+                peaks[i] = 0          // No signal
+                filteredYmean[i] = y[i]
+                filteredYstd[i] = y[i]
             }
-            
             // Adjust the filters
-            avgFilter[i] = getMedian(array: subArray(array: filteredYmean, s: i-_averagingLag, e: i))
-            stdFilter[i] = standardDeviation(array: subArray(array: filteredYstd, s: i-_lagDeviation, e: i))
+            avgFilter[i] = getMean(array: subArray(array: filteredYmean, s: i-_averagingLag, e: i))
+            devFilter[i] = getStandardDeviation(array: subArray(array: filteredYstd, s: i-_deviationLag, e: i))
         }
 
-        return XvPeakDetectionPacket(peaks: peaks, averagedValues: avgFilter, standardDeviationValues: stdFilter)
-        
+        return XvPeakDetectionPacket(peaks: peaks, averagedValues: avgFilter, deviationValues: devFilter)
+
     }
     
     //MARK: - Helpers -
@@ -110,12 +234,25 @@ public class XvPeakDetection {
     }
     
     // Function to calculate the standard deviation
-    fileprivate func standardDeviation(array:[Double]) -> Double {
+    fileprivate func getStandardDeviation(array:[Double]) -> Double {
         
         let length:Double = Double(array.count)
         let avg:Double = array.reduce(0, {$0 + $1}) / length
         let sumOfSquaredAvgDiff:Double = array.map { pow($0 - avg, 2.0)}.reduce(0, {$0 + $1})
         return sqrt(sumOfSquaredAvgDiff / length)
+    }
+    
+    fileprivate func getMeanAbsoluteDeviation(array:[Double]) -> Double {
+        
+        //get mean of the array
+        let mean:Double = getMean(array: array)
+        
+        //calculate distance between each number in area from the mean
+        //but make all values positive (absolute value)
+        let absoluteDeviations:[Double] = array.map { abs($0-mean) }
+        
+        //get the average (mean) absolute deviation
+        return absoluteDeviations.reduce(0, +) / Double(absoluteDeviations.count)
     }
     
     // Function to extract some range from an array
@@ -126,7 +263,24 @@ public class XvPeakDetection {
         return Array(array[s..<min(e, array.count)])
     }
     
-    //MARK: Vars
+    fileprivate func _bufferSignal() -> Bool{
+        
+        if (_signal.count < _bins) {
+            print("XvPeakDetection: Building buffer", _signal.count, "/", _bins)
+            return false
+        }
+        
+        //if signal goes above bin length
+        if (_signal.count > _bins) {
+            
+            //remove the excess from the beginning of the array
+            _signal.removeFirst(_signal.count-_bins)
+        }
+        return true
+    }
+    
+    //MARK: - Acccessors -
+    fileprivate let influenceAttn:XvAttenuator = XvAttenuator(min: 0.0, max: 1.0)
     fileprivate var _threshold:Double
     public var threshold:Double {
         get { return _threshold }
@@ -139,22 +293,22 @@ public class XvPeakDetection {
         set { _averagingLag = newValue }
     }
     
-    fileprivate var _lagDeviation:Int
-    public var lagDeviation:Int {
-        get { return _lagDeviation }
-        set { _lagDeviation = newValue }
+    fileprivate var _deviationLag:Int
+    public var deviationLag:Int {
+        get { return _deviationLag }
+        set { _deviationLag = newValue }
     }
     
     fileprivate var _averagingInfluence:Double
     public var averagingInfluence:Double {
         get { return _averagingInfluence }
-        set { _averagingInfluence = newValue }
+        set { _averagingInfluence = influenceAttn.attenuate(value: newValue) }
     }
     
     fileprivate var _deviationInfluence:Double
     public var deviationInfluence:Double {
         get { return _deviationInfluence }
-        set { _deviationInfluence = newValue }
+        set { _deviationInfluence = influenceAttn.attenuate(value: newValue) }
     }
     
 }
